0c9cd6677f51107d1fa8920c215471721176d1c4
EYoung75 <eyoung75.ey@gmail.com>
2019-10-22 20:59:34 -0400

    added dismissible widget to wrap cart items with functionality for deleting item from cart

--- lib/screens/cartScreen.dart
+++ lib/screens/cartScreen.dart
@@ -62,6 +62,7 @@ class CartScreen extends StatelessWidget {
               itemCount: cart.items.length,
               itemBuilder: (ctx, i) => ci.CartItem(
                 cart.items.values.toList()[i].id,
+                cart.items.keys.toList()[i],
                 cart.items.values.toList()[i].title,
                 cart.items.values.toList()[i].price,
                 cart.items.values.toList()[i].quantity,
--- lib/widgets/cartItem.dart
+++ lib/widgets/cartItem.dart
@@ -1,13 +1,18 @@
 import "package:flutter/material.dart";
 
+import "package:provider/provider.dart";
+import "../providers/cart.dart";
+
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final String title;
   final double price;
   final int quantity;
 
   CartItem(
     this.id,
+    this.productId,
     this.title,
     this.price,
     this.quantity,
@@ -15,22 +20,44 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 5,
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      key: ValueKey(id),
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(9),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(9),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
                 padding: EdgeInsets.all(5),
-                child: FittedBox(child: Text("\$$price"))),
+                child: FittedBox(child: Text("\$$price")),
+              ),
+            ),
+            title: Text(title),
+            subtitle: Text("Total: \$${(quantity * price)}"),
+            trailing: Text("$quantity x"),
           ),
-          title: Text(title),
-          subtitle: Text("Total: \$${(quantity * price)}"),
-          trailing: Text("$quantity x"),
         ),
       ),
     );
