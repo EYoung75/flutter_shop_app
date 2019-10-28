9099da04888a2a066ae76026ea0178d35134b97e
EYoung75 <eyoung75.ey@gmail.com>
2019-10-27 21:21:26 -0400

    another very critical commit

--- lib/screens/ordersScreen.dart
+++ lib/screens/ordersScreen.dart
@@ -11,7 +11,7 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       appBar: AppBar(
         title: Text("Your orders"),
       ),
